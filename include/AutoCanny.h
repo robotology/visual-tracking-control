f2b3a0cb5f13397da8456f2a505728a528b943e5
Claudio <claudio.fantacci@gmail.com>
2016-11-25 14:24:10 +0100

    Add first implementation of directed edges
    AutoCanny.h has been significantly improved.
    In particular, the following improvements have been done.
     - New AutoDirCanny: the function returns a floating point, single
       channel, edge image. Each edge pixel value is
       equivalent to its angle direction. Thus, instead of being
       1 (or equivalently 255) edge pixels, the corresponding
       value equals the angle at which that pixel was detected as an
       edge (i.e. the direction of the image gradient).
     - Optimized the code during edge detection to avoid any memory copy.

--- include/AutoCanny.h
+++ include/AutoCanny.h
@@ -1,9 +1,10 @@
 #ifndef AUTOCANNY_H
 #define AUTOCANNY_H
 
-#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
+
 
 const double percent_of_pixels_not_edges = 0.7;     // MATLAB: Used for selecting thresholds
 const double threshold_ratio             = 0.4;     // MATLAB: Low threshold is this fraction of the high one
@@ -22,25 +23,12 @@ void CumSum(const Mat & histogram, Mat & cumsum)
 }
 
 
-void AutoCanny(const Mat & src, Mat & dst) {
-    int filter_length = 8 * static_cast<int>(ceil(sigma));
-    int n             = (filter_length - 1)/2;
-    Mat GX;
-    Mat GY;
+void ImgGradient(const Mat & src, Mat & GX, Mat & GY)
+{
+    const int filter_length = 8 * static_cast<int>(ceil(sigma));
+    const int n             = (filter_length - 1)/2;
     Mat gauss_filter_transpose;
     Mat gradient_gauss_filter_transpose;
-    Mat mag_gradient;
-    Mat mag_hist_3c;
-    Mat mag_hist_cumsum_3c;
-    const int channels[]      = {2};
-    const float range[]       = {0.0, 1.01};
-    const int hist_size[]     = {64};
-    const float* hist_range[] = {range};
-    double percentile = percent_of_pixels_not_edges * src.cols * src.rows;
-    double high_threshold;
-    double low_threshold;
-    Mat edge;
-
 
     Mat gauss_filter = getGaussianKernel(filter_length, sigma);
     normalize(gauss_filter, gauss_filter, 1.0, 0.0, NORM_L1);
@@ -66,33 +54,122 @@ void AutoCanny(const Mat & src, Mat & dst) {
 
     filter2D(src, GY, CV_32F, gauss_filter_transpose,          Point(n, 0), 0, BORDER_REPLICATE);
     filter2D(GY,  GY, CV_32F, gradient_gauss_filter ,          Point(0, n), 0, BORDER_REPLICATE);
+}
 
+
+void AutoThreshold(const Mat & GX, const Mat & GY, double & high, double & low)
+{
+    Mat              mag_gradient;
+    Mat              mag_hist;
+    Mat              mag_hist_cumsum;
+    const int        channels[]   = {2};
+    const int        hist_size[]  = {64};
+    const float      range[]      = {0.0, 1.01};
+    const float    * hist_range[] = {range};
+    const double     percentile   = percent_of_pixels_not_edges * GX.cols * GX.rows;
+
+    /* Hypot intensity gradient computation [1] */
+    /* [1] https://en.wikipedia.org/wiki/Hypot  */
     Mat GX_abs = abs(GX);
     Mat GY_abs = abs(GY);
-    Mat G_t = min(GX_abs, GY_abs);
-    Mat G_x = max(GX_abs, GY_abs);
-
+    Mat G_t    = min(GX_abs, GY_abs);
+    Mat G_x    = max(GX_abs, GY_abs);
     divide(G_t, G_x, G_t);
     pow(G_t, 2.0, G_t);
     sqrt(Scalar(1.0, 1.0, 1.0) + G_t, mag_gradient);
     multiply(G_x, mag_gradient, mag_gradient);
 
+    /* Histogram-based automatic threashold computation using image intensity gradient */
+    if (mag_gradient.channels() > 1)
+    {
+        MatIterator_<Vec3f> it   = mag_gradient.begin<Vec3f>();
+        MatIterator_<Vec3f> last = mag_gradient.end<Vec3f>();
+        Scalar max_values = *it;
+
+        while (++it!=last)
+        {
+            Scalar max_cart = *it;
+            if (max_values(0) < max_cart(0)) max_values(0) = max_cart(0);
+            if (max_values(1) < max_cart(1)) max_values(1) = max_cart(1);
+            if (max_values(2) < max_cart(2)) max_values(2) = max_cart(2);
+        }
+        divide(mag_gradient, max_values, mag_gradient);
+    }
+    else
+    {
+        double min_gradient;
+        double max_gradient;
+
+        minMaxLoc(mag_gradient, &min_gradient, &max_gradient);
+        mag_gradient /= max_gradient;
+    }
+    calcHist(&mag_gradient, 1, channels, Mat(), mag_hist, 1, hist_size, hist_range);
+    CumSum(mag_hist, mag_hist_cumsum);
+    MatIterator_<float> up_bgr;
+    up_bgr = std::upper_bound(mag_hist_cumsum.begin<float>(), mag_hist_cumsum.end<float>(), percentile);
+    high = (static_cast<double>(up_bgr.lpos()) + 1.0) / static_cast<double>(hist_size[0]) * 255.0;
+    low  = threshold_ratio * high;
+}
+
+
+void AutoCanny(const Mat & src, Mat & dst)
+{
+    Mat GX;
+    Mat GY;
+    double high_threshold;
+    double low_threshold;
+
+
+    ImgGradient(src, GX, GY);
+
+    AutoThreshold(GX, GY, high_threshold, low_threshold);
+
+    /* OpenCV Canny edge of an image using custom image gradient [1] */
+    /* [1] http://docs.opencv.org/master/dd/d1a/group__imgproc__feature.html#ga2a671611e104c093843d7b7fc46d24af */
     GX.convertTo(GX, CV_16SC3);
     GY.convertTo(GY, CV_16SC3);
-
-    //???: indagare accuratamente il calcolo dell'istogramma su più canali e come vengono calcolati i threshold
-    double min;
-    double max;
-    minMaxLoc(mag_gradient, &min, &max);
-    mag_gradient /= max;
-    calcHist(&mag_gradient, 1, channels, Mat(), mag_hist_3c, 1, hist_size, hist_range);
-    CumSum(mag_hist_3c, mag_hist_cumsum_3c);
-    MatIterator_<float> up_bgr;
-    up_bgr = std::upper_bound(mag_hist_cumsum_3c.begin<float>(), mag_hist_cumsum_3c.end<float>(), percentile);
-    high_threshold = (static_cast<double>(up_bgr.lpos()) + 1.0) / static_cast<double>(hist_size[0]) * 255.0;
-    low_threshold  = threshold_ratio * high_threshold;
-
     Canny(GX, GY, dst, low_threshold, high_threshold, true);
+}
+
+
+void AutoDirCanny(const Mat & src, Mat & dst)
+{
+    Mat GX;
+    Mat GY;
+    Mat dir;
+    Mat dst_cart;
+    double high_threshold;
+    double low_threshold;
+
+
+    ImgGradient(src, GX, GY);
+
+    AutoThreshold(GX, GY, high_threshold, low_threshold);
+
+    /* OpenCV Canny directed edge of an image using custom image gradient [1] */
+    /* [1] http://docs.opencv.org/master/dd/d1a/group__imgproc__feature.html#ga2a671611e104c093843d7b7fc46d24af */
+    if (GX.channels() > 1)
+    {
+        Mat GX_avg(GX.rows, GX.cols, CV_32FC1);
+        Mat GY_avg(GY.rows, GY.cols, CV_32FC1);
+        for (size_t i = 0; i < GX.rows; ++i)
+        {
+            for (size_t j = 0; j < GX.cols; ++j)
+            {
+                GX_avg.at<float>(i, j) = (GX.at<Vec3f>(i, j)[0] + GX.at<Vec3f>(i, j)[1] + GX.at<Vec3f>(i, j)[2]) / 3.0;
+                GY_avg.at<float>(i, j) = (GY.at<Vec3f>(i, j)[0] + GY.at<Vec3f>(i, j)[1] + GY.at<Vec3f>(i, j)[2]) / 3.0;
+            }
+        }
+        phase(GX_avg, GY_avg, dir);
+    }
+    else phase(GX, GY, dir);
+
+    GX.convertTo(GX, CV_16SC3);
+    GY.convertTo(GY, CV_16SC3);
+    Canny(GX, GY, dst_cart, low_threshold, high_threshold, true);
+
+    normalize(dst_cart, dst_cart, 0.0, 1.0, NORM_MINMAX);
+    dir.copyTo(dst, dst_cart);
 }
 
 #endif /* AUTOCANNY_H */
