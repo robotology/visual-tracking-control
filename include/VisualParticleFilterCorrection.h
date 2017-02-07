8efc13bfa070a29f96ca6b08f67e9f448180bd74
Claudio <claudio.fantacci@gmail.com>
2017-02-07 17:41:13 +0100

    Add threading capability to VisualPF correction
    A brand new implementation of the VisualParticleFilterCorrection
    has been implemented.
    
    In particular, at runtime, a number of threads equal to
    the number of CUDA streams used by HOG are deployed.
    In this way, each thread invokes, in parallel, the HOG kernels
    on a different CUDA stream.
    
    Code is still under profiling. The current implementation does not
    provide the level of concurrency required to have significant
    benefits over a serial implementation.

--- include/VisualParticleFilterCorrection.h
+++ include/VisualParticleFilterCorrection.h
@@ -1,6 +1,10 @@
 #ifndef VISUALPARTICLEFILTERCORRECTION_H
 #define VISUALPARTICLEFILTERCORRECTION_H
 
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
 #include <BayesFiltersLib/VisualCorrection.h>
 #include <BayesFiltersLib/VisualObservationModel.h>
 #include <opencv2/core/cuda.hpp>
@@ -38,6 +42,8 @@ public:
 
     void innovation(const Eigen::Ref<const Eigen::MatrixXf>& pred_state, cv::InputArray measurements, Eigen::Ref<Eigen::MatrixXf> innovation) override;
 
+    virtual void parallel_innovation(const int stream_id);
+
     void likelihood(const Eigen::Ref<const Eigen::MatrixXf>& innovation, Eigen::Ref<Eigen::MatrixXf> cor_state) override;
 
 protected:
@@ -47,12 +53,20 @@ protected:
     const int                                    bin_number_ = 9;
     const int                                    img_width_  = 320;
     const int                                    img_height_ = 240;
-    cv::HOGDescriptor                            hog_;
+    cv::HOGDescriptor                            hog_; // FIXME: confront output before deleting
     cv::Ptr<cv::cuda::HOG>                       cuda_hog_;
 
+    bool                                         is_running_ = true;
+    std::vector<bool>                            cuda_go_;
+    std::vector<std::mutex>                      cuda_mutex_;
+    std::vector<std::unique_lock<std::mutex>>    cuda_ulock_;
+    std::vector<std::condition_variable>         cuda_condvar_;
+    std::vector<std::thread>                     cuda_thread_;
+    const Eigen::Ref<const Eigen::MatrixXf>*     pred_state_;
     const int                                    num_particle_;
     const int                                    num_image_stream_;
-    std::vector<cv::cuda::Stream>                stream_;
+    std::vector<cv::Mat>                         hand_rendered_;
+    std::vector<cv::cuda::Stream>                cuda_stream_;
     std::vector<cv::cuda::GpuMat>                cuda_img_;
     std::vector<cv::cuda::GpuMat>                cuda_img_alpha_;
     std::vector<cv::cuda::GpuMat>                cuda_descriptors_;
--- src/VisualParticleFilterCorrection.cpp
+++ src/VisualParticleFilterCorrection.cpp
@@ -1,6 +1,7 @@
 #include "VisualParticleFilterCorrection.h"
 
 #include <cmath>
+#include <functional>
 #include <iostream>
 #include <utility>
 #include <vector>
@@ -24,25 +25,37 @@ VisualParticleFilterCorrection::VisualParticleFilterCorrection(std::shared_ptr<V
 VisualParticleFilterCorrection::VisualParticleFilterCorrection(std::shared_ptr<VisualObservationModel> measurement_model, const int num_particle, const int num_cuda_stream) noexcept :
     measurement_model_(measurement_model),
     hog_(HOGDescriptor(Size(img_width_, img_height_), Size(block_size_, block_size_), Size(block_size_/2, block_size_/2), Size(block_size_/2, block_size_/2), bin_number_, 1, -1, HOGDescriptor::L2Hys, 0.2, true, HOGDescriptor::DEFAULT_NLEVELS, false)),
-    num_particle_(num_particle), num_image_stream_(num_particle/num_cuda_stream), stream_(num_cuda_stream)
+    cuda_go_(num_cuda_stream, false), cuda_mutex_(num_cuda_stream), cuda_condvar_(num_cuda_stream), num_particle_(num_particle), num_image_stream_(num_particle/num_cuda_stream), cuda_stream_(num_cuda_stream)
 {
     cuda_hog_ = cuda::HOG::create(Size(img_width_, img_height_), Size(block_size_, block_size_), Size(block_size_/2, block_size_/2), Size(block_size_/2, block_size_/2), bin_number_);
     cuda_hog_->setDescriptorFormat(cuda::HOG::DESCR_FORMAT_COL_BY_COL);
     cuda_hog_->setGammaCorrection(true);
     cuda_hog_->setWinStride(Size(img_width_, img_height_));
 
-    const int descriptor_length = (img_width_/block_size_*2-1) * (img_height_/block_size_*2-1) * bin_number_ * 4;
-    std::cout << descriptor_length << std::endl;
+    const unsigned int descriptor_length = (img_width_/block_size_*2-1) * (img_height_/block_size_*2-1) * bin_number_ * 4;
     for (int i = 0; i < num_cuda_stream; ++i)
     {
+        cuda_ulock_.insert      (cuda_ulock_.begin(),       std::unique_lock<std::mutex>(cuda_mutex_[i], std::defer_lock));
+        cuda_thread_.insert     (cuda_thread_.begin(),      std::thread(&VisualParticleFilterCorrection::parallel_innovation, this, i));
         cuda_img_.insert        (cuda_img_.begin(),         cuda::GpuMat(Size(img_width_ * num_image_stream_, img_height_), CV_8UC3));
         cuda_img_alpha_.insert  (cuda_img_alpha_.begin(),   cuda::GpuMat(Size(img_width_ * num_image_stream_, img_height_), CV_8UC4));
         cuda_descriptors_.insert(cuda_descriptors_.begin(), cuda::GpuMat(Size(num_image_stream_, descriptor_length),        CV_32F ));
+        hand_rendered_.insert   (hand_rendered_.begin(),    Mat(Size(img_width_ * num_image_stream_, img_height_),          CV_8UC3));
     }
 }
 
 
-VisualParticleFilterCorrection::~VisualParticleFilterCorrection() noexcept { }
+VisualParticleFilterCorrection::~VisualParticleFilterCorrection() noexcept
+{
+    is_running_ = false;
+    for (int i = 0; i < cuda_thread_.size(); ++i){
+        cuda_ulock_  [i].lock();
+        cuda_go_     [i] = true;
+        cuda_ulock_  [i].unlock();
+        cuda_condvar_[i].notify_one();
+        cuda_thread_ [i].detach();
+    }
+}
 
 
 VisualParticleFilterCorrection::VisualParticleFilterCorrection(const VisualParticleFilterCorrection& vpf_correction) :
@@ -77,34 +90,42 @@ VisualParticleFilterCorrection& VisualParticleFilterCorrection::operator=(Visual
 void VisualParticleFilterCorrection::correct(const Ref<const MatrixXf>& pred_state, InputArray measurements, Ref<MatrixXf> cor_state)
 {
     VectorXf innovate(pred_state.cols());
-    innovation(pred_state, measurements, innovate);
+
+    Rect current_ROI(0, 0, img_width_, img_height_);
+    for (int i = 0; i < cuda_stream_.size(); ++i)
+    {
+        for (int j = 0; j < num_image_stream_; ++j)
+        {
+            current_ROI.x = j * img_width_;
+            measurement_model_->observe(pred_state.col((i * num_image_stream_) + j), hand_rendered_[i](current_ROI));
+        }
+    }
+
+    pred_state_ = &pred_state;
+    for (int i = 0; i < cuda_stream_.size(); ++i)
+    {
+        cuda_ulock_  [i].lock();
+        cuda_go_     [i] = true;
+        cuda_ulock_  [i].unlock();
+        cuda_condvar_[i].notify_one();
+    }
+
+    innovation(MatrixXf(), measurements, innovate);
+
     likelihood(innovate, cor_state);
 }
 
 
 void VisualParticleFilterCorrection::innovation(const Ref<const MatrixXf>& pred_state, InputArray measurements, Ref<MatrixXf> innovation)
 {
-    Mat       hand_ogl_cv(Size(img_width_ * num_image_stream_, img_height_), CV_8UC3);
-    Rect      current_ROI(0, 0, img_width_, img_height_);
-    Mat       descriptors_cad;
+    Mat descriptors_cad;
 
-    for (int i = 0; i < stream_.size(); ++i)
+    for (int i = 0; i < cuda_stream_.size(); ++i)
     {
-        for (int j = 0; j < num_image_stream_; ++j)
-        {
-            current_ROI.x = j * img_width_;
-            measurement_model_->observe(pred_state.col((i * num_image_stream_) + j), hand_ogl_cv(current_ROI));
-        }
-
-        cuda_img_[i].upload(hand_ogl_cv, stream_[i]);
-        cuda::cvtColor(cuda_img_[i], cuda_img_alpha_[i], COLOR_BGR2BGRA, 4, stream_[i]);
-        cuda_hog_->compute(cuda_img_alpha_[i], cuda_descriptors_[i], stream_[i]);
-    }
-
-    for (int i = 0; i < stream_.size(); ++i)
-    {
-        stream_[i].waitForCompletion();
-        cuda_descriptors_[i].download(descriptors_cad, stream_[i]);
+        cuda_stream_[i].waitForCompletion();
+        cuda_ulock_[i].lock();
+        cuda_descriptors_[i].download(descriptors_cad, cuda_stream_[i]);
+        cuda_stream_[i].waitForCompletion();
 
         for (int j = 0; j < num_image_stream_; ++j)
         {
@@ -118,6 +139,30 @@ void VisualParticleFilterCorrection::innovation(const Ref<const MatrixXf>& pred_
 
             innovation((i * num_image_stream_) + j, 0) = sum_diff;
         }
+
+        cuda_ulock_[i].unlock();
+    }
+}
+
+
+void VisualParticleFilterCorrection::parallel_innovation(const int stream_id)
+{
+    std::unique_lock<std::mutex> local_ulock(cuda_mutex_[stream_id]);
+
+    while (is_running_) {
+        while (!cuda_go_[stream_id]) cuda_condvar_[stream_id].wait(local_ulock);
+        if    (!is_running_)
+        {
+            cuda_go_[stream_id] = false;
+            local_ulock.unlock();
+            break;
+        }
+
+        cuda_img_[stream_id].upload(hand_rendered_[stream_id], cuda_stream_[stream_id]);
+        cuda::cvtColor(cuda_img_[stream_id], cuda_img_alpha_[stream_id], COLOR_BGR2BGRA, 4, cuda_stream_[stream_id]);
+        cuda_hog_->compute(cuda_img_alpha_[stream_id], cuda_descriptors_[stream_id], cuda_stream_[stream_id]);
+
+        cuda_go_[stream_id] = false;
     }
 }
 
