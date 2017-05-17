bef24b7c0f1fb408b0c6f3c8ad0bf21c3763f43a
Claudio <claudio.fantacci@gmail.com>
2017-05-17 15:39:13 +0200

    Change velocity control gain

--- src/reaching_pose/main.cpp
+++ src/reaching_pose/main.cpp
@@ -355,7 +355,7 @@ public:
 
         double Ts    = 0.1;   // controller's sample time [s]
         double K_x   = 0.5;  // visual servoing proportional gain
-        double K_o   = 0.5;  // visual servoing proportional gain
+        double K_o   = 0.25;  // visual servoing proportional gain
 //        double v_max = 0.0005; // max cartesian velocity [m/s]
 
         bool done = false;
