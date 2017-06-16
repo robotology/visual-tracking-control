f352477d62503965781078c188f55427f95c9b19
Claudio <claudio.fantacci@gmail.com>
2017-06-16 09:20:36 +0200

    Increase history window to 60

--- src/hand-tracking/include/VisualSIRParticleFilter.h
+++ src/hand-tracking/include/VisualSIRParticleFilter.h
@@ -45,7 +45,7 @@ public:
     bool            setHistorySize(const unsigned int window);
 
 private:
-    unsigned int                window_     = 30;
+    unsigned int                window_     = 60;
 
     const unsigned int          max_window_ = 90;
 
