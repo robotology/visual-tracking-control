edcb514df4e1c8c988f32caf31a57095497fa8f5
Claudio Fantacci <claudio.fantacci@gmail.com>
2017-11-01 18:09:14 +0100

    Rename and update DrawParticlesPose class
    The new class accounts for exogenous inputs and the new classes
    provided by the BayesFilters library.
    
    WIP. The code does not compile.

--- src/hand-tracking/include/DrawFwdKinPoses.h
+++ src/hand-tracking/include/DrawFwdKinPoses.h
@@ -1,5 +1,5 @@
-#ifndef DRAWPARTICLESPOSE_H
-#define DRAWPARTICLESPOSE_H
+#ifndef DRAWFWDKINPOSES_H
+#define DRAWFWDKINPOSES_H
 
 #include <memory>
 #include <random>
@@ -8,31 +8,37 @@
 #include <BayesFilters/StateModel.h>
 
 namespace bfl {
-    class DrawParticlesPose;
+    class DrawFwdKinPoses;
 }
 
 
-class bfl::DrawParticlesPose : public bfl::PFPrediction
+class bfl::DrawFwdKinPoses : public bfl::PFPrediction
 {
 public:
-    DrawParticlesPose() noexcept;
+    DrawFwdKinPoses() noexcept;
 
-    DrawParticlesPose(DrawParticlesPose&& pf_prediction) noexcept;
+    DrawFwdKinPoses(DrawFwdKinPoses&& pf_prediction) noexcept;
 
-    ~DrawParticlesPose() noexcept;
+    ~DrawFwdKinPoses() noexcept;
 
-    DrawParticlesPose& operator=(DrawParticlesPose&& pf_prediction) noexcept;
+    DrawFwdKinPoses& operator=(DrawFwdKinPoses&& pf_prediction) noexcept;
 
 
     StateModel& getStateModel() override;
 
     void setStateModel(std::unique_ptr<StateModel> state_model) override;
 
+    ExogenousModel& getExogenousModel() override;
+
+    void setExogenousModel(std::unique_ptr<ExogenousModel> exogenous_model) override;
+
 protected:
     void predictStep(const Eigen::Ref<const Eigen::MatrixXf>& prev_states, const Eigen::Ref<const Eigen::VectorXf>& prev_weights,
                      Eigen::Ref<Eigen::MatrixXf> pred_states, Eigen::Ref<Eigen::VectorXf> pred_weights) override;
 
     std::unique_ptr<StateModel> state_model_;
+
+    std::unique_ptr<ExogenousModel> exogenous_model_;
 };
 
-#endif /* DRAWPARTICLESPOSE_H */
+#endif /* DRAWFWDKINPOSES_H */
