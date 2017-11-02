ef5a68980f07360b46c5bfc09ebc517fd57614d1
Claudio Fantacci <claudio.fantacci@gmail.com>
2017-11-02 11:13:10 +0100

    Fix skip options
    State and exogenous options were missing.

--- src/hand-tracking/src/VisualSIS.cpp
+++ src/hand-tracking/src/VisualSIS.cpp
@@ -47,7 +47,9 @@ VisualSIS::VisualSIS(const ConstString& cam_sel, const ConstString& laterality, 
 
 bool VisualSIS::skip(const std::string& what_step, const bool status)
 {
-    if (what_step == "prediction")
+    if (what_step == "prediction" ||
+        what_step == "state"      ||
+        what_step == "exogenous"    )
         return prediction_->skip(what_step, status);
 
     if (what_step == "correction")
