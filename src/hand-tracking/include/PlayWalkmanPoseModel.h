b7db6a6167f811c2f39b29d7d89c4e7b290855d2
Claudio Fantacci <claudio.fantacci@gmail.com>
2018-02-12 15:30:34 +0100

    Add first implementation of PlayWalkmanPoseModel

---
+++ src/hand-tracking/include/PlayWalkmanPoseModel.h
@@ -0,0 +1,38 @@
+#ifndef PLAYWALKMANPOSEMODEL_H
+#define PLAYWALKMANPOSEMODEL_H
+
+#include <KinPoseModel.h>
+
+#include <iCub/iKin/iKinFwd.h>
+#include <yarp/os/Bottle.h>
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/ConstString.h>
+#include <yarp/sig/Vector.h>
+
+
+class PlayWalkmanPoseModel : public KinPoseModel
+{
+public:
+    PlayWalkmanPoseModel(const yarp::os::ConstString& robot, const yarp::os::ConstString& laterality, const yarp::os::ConstString& port_prefix) noexcept;
+
+    ~PlayWalkmanPoseModel() noexcept;
+
+    bool setProperty(const std::string& property) override;
+
+protected:
+    Eigen::VectorXd   readPose() override;
+
+    yarp::sig::Vector readRootToEE();
+
+    yarp::os::BufferedPort<yarp::os::Bottle> port_arm_pose_;
+
+private:
+    yarp::os::ConstString ID_     = "PlayWalkmanPoseModel";
+    yarp::os::ConstString log_ID_ = "[" + ID_ + "]";
+
+    yarp::os::ConstString robot_;
+    yarp::os::ConstString laterality_;
+    yarp::os::ConstString port_prefix_;
+};
+
+#endif /* PLAYWALKMANPOSEMODEL_H */
