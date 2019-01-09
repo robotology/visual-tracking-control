468cbcff9109bd7d99940c087f7b774818f6b1f4
Nicola Piga <nicolapiga@gmail.com>
2019-01-09 13:32:47 +0100

    Use method MeasurementModel bfl::measure instead of bfl::MeasurementModel::getAgentMeasurements() in NormTwo::likelihood
    This modifies NormTwo for execution on the CPU

--- src/hand-tracking/src/cpu/NormTwo.cpp
+++ src/hand-tracking/src/cpu/NormTwo.cpp
@@ -37,7 +37,7 @@ std::pair<bool, VectorXd> NormTwo::likelihood(const MeasurementModel& measuremen
 {
     bool valid_measurements;
     Data data_measurements;
-    std::tie(valid_measurements, data_measurements) = measurement_model.getAgentMeasurements();
+    std::tie(valid_measurements, data_measurements) = measurement_model.measure();
 
     if (!valid_measurements)
         return std::make_pair(false, VectorXd::Zero(1));
